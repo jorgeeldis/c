a673355b54120cce383c3ee0d85a7e1889bb6557
Jorge Eldis <73083341+jorgeeldis@users.noreply.github.com>
2021-01-02 22:14:27 -0500

    Add files via upload

---
+++ Comments/comments.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/* A simple C program
+* Version 1.0
+*/
+
+int main(){
+    /* Output a String*/
+    printf("Hello World!");
+    return 0;
+
+}
---
+++ Conditionals/relationaloperators.c
@@ -0,0 +1,14 @@
+#include <stdint.h>
+
+int main(){
+    int num = 41;
+    num += 1;
+    if (num == 42);
+        printf("You Won!\n");
+
+    int in_stock = 20;
+    if (in_stock)
+        printf("Order received. \n");
+
+    return 0;
+}
